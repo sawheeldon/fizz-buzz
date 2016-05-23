b55b60b1e30d50dee192f75024fbba854dec3210
Simon Wheeldon <simon@jampot.design>
2016-05-23 16:39:12 +0100

    Working through JS

--- js/fizz-buzz-refactor.js
+++ js/fizz-buzz-refactor.js
@@ -5,19 +5,49 @@
 - PART 3.1 => count from 1 to the set limit
 - PART 3.2 => for each number check it and output the result
 */
-function val (limit){
+function validateInput(inputNumber) {
+
+    var userInputCorrectFlag = true;
+
+    while (inputNumber.length < 1) {
+        var inputNumber = prompt("Please enter 1 or more digit.");
+        userInputCorrectFlag = false;
+    }
+    while (inputNumber.indexOf(' ') > 0) {
+        var inputNumber = prompt("Please try with no spacing, just enter a number.");
+        userInputCorrectFlag = false;
+    }
+    while (Math.floor(inputNumber) != inputNumber) {
+        var inputNumber = prompt("Your upper limit was not a number, please try again.");
+        userInputCorrectFlag = false;
+    }
+
+    if (userInputCorrectFlag = true) {
+        playFizzBuzz
+    }
+}
+
+function playFizzBuzz(input) {
+    for (var counter = 1; counter <= limit; counter++) {
+        $('#showResults').append(checkFizzBuzz(counter));
+    }
 
 }
 
-function play (){
+function checkFizzBuzz() {
 
 }
 
-function check (){
-
+function input() {
+    //alert("This works");
+    prompt("Please set the highest number for the game");
 }
 
-$(document).ready(function(){
-    var limit = prompt("Set up a limit");
-    val (limit);
+$(document).ready(function () {
+
+    $('#click-me').click(function () {
+        input();
+
+        validateInput(input);
+    });
 });
