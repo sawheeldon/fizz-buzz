ab59d3bca8075c6c7dede4a086a47598234f85e6
Simon Wheeldon <simon@jampot.design>
2016-05-23 12:32:28 +0100

    Fizz-Buzz decoration

---
+++ js/fizz-buzz-refactor.js
@@ -0,0 +1,23 @@
+/* this app has the following parts
+- PART 1 => prompt for a number
+- PART 2 => validate input
+- PART 3 => if the input is valid play the game
+- PART 3.1 => count from 1 to the set limit
+- PART 3.2 => for each number check it and output the result
+*/
+function val (limit){
+
+}
+
+function play (){
+
+}
+
+function check (){
+
+}
+
+$(document).ready(function(){
+    var limit = prompt("Set up a limit");
+    val (limit);
+});
